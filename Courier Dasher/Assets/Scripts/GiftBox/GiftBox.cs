5ca499b59fae808caadc6e54d23fc221be3f506f
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-13 11:31:12 +0530

    Player movement and animations

---
+++ Courier Dasher/Assets/Scripts/GiftBox/GiftBox.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GiftBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
